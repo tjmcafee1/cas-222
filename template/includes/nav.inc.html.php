fa6524796b7f1778d4f5ae5441cb83bea545cbb6
tjmcafee1 <46771455+tjmcafee1@users.noreply.github.com>
2019-02-12 00:06:11 -0800

    Revision 2

--- template/includes/nav.inc.html.php
+++ template/includes/nav.inc.html.php
@@ -6,9 +6,10 @@
             <div></div>
         </dt>
         <dd>
-            <p><a id="link1" href="#">Lorem Ipsum</a></p>
-            <p><a id="link2" href="#section2">Lorem Ipsum</a></p>
-            <p><a id="link2" href="#section3">Lorem Ipsum</a></p>
+            <p><a id="link1" href="#">Home</a></p>
+            <p><a id="link2" href="#section1">About</a></p>
+            <p><a id="link3" href="#section2">Registration</a></p>
+            <p><a id="link4" href="#section3">FAQ</a></p>
         </dd>
     </dl>
 </nav>
