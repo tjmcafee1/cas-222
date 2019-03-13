33e247e0efe194a30f229ca249fd098b0365efb8
tjmcafee1 <46771455+tjmcafee1@users.noreply.github.com>
2019-03-12 18:41:06 -0700

    Final
    Final Project folder for CAS222

---
+++ final/success.html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Success</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=styles/ace.css>
+</head>
+<body>
+<?php include 'includes/header.inc.html.php'; ?>
+<?php include 'includes/nav.inc.html.php'; ?>
+<p>You have submitted your information.</p>
+<?php include 'includes/footer.inc.html.php'; ?>
+</body>
+</html>
